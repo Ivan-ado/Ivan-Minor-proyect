53c9559b8631f12d9c21ffa58c686cc47717c28e
Ivan-ado <61509992+Ivan-ado@users.noreply.github.com>
2020-11-10 22:18:58 -0600

    segunda mitad proyecto
    la segunda parte del código para el primer avance del proyecto, redactado por minor aunque subido por Ivan Rojas por problemas de coneccion del primer desarrollador

---
+++ proyecto_minor.py
@@ -0,0 +1,280 @@
+def historial_consultas(faces):
+    historial=[]
+    for face in faces:
+        dicc={}
+        faceId=face['faceId']
+        faceAttributes=face['faceAttributes']
+        gender=faceAttributes['gender']
+        age=faceAttributes['age']
+        emotion=faceAttributes['emotion']
+        faceRectangle=face['faceRectangle']
+        dicc['faceId']= faceId
+        dicc['gender']= gender
+        dicc['age']= age
+        dicc['emotion']= emotion
+        dicc['faceRectangle']= faceRectangle
+        historial.append(dicc)
+    return historial
+    
+    
+    def young_emotions_rec(picture, faces): 
+    mas_joven=150
+    faceId_real=""
+    emociones= {}
+    rostro_joven={}
+    for face in faces:
+        faceId=face['faceId']
+        faceRectangle=face['faceRectangle']
+        emotion=face['emotion']
+        age=face['age']
+        if age < mas_joven:
+            mas_joven=age
+            faceId_real= faceId
+            emociones=emotion
+            rostro_joven=face
+    print('\nLa persona más joven es:', faceId_real)
+    print('Edad:',mas_joven)
+    print('Emociones: ')
+    for x, y in emociones.items():
+            print('\t',x + ':', y)
+    show_image(picture, [rostro_joven]) 
+    
+    
+ def  show_age_and_gender_quick(faces):
+
+    ages_faces=[]
+    for face in faces: 
+        temporal={}                       
+        faceAttributes=face['faceAttributes']
+        edad=faceAttributes['age']
+        genero=faceAttributes['gender']
+        temporal['age']= edad
+        temporal['gender']= genero
+        ages_faces.append(temporal)
+    male=[]
+    female=[]
+    for genero in ages_faces:
+        if genero['gender']=='female':
+            female.append(genero)
+        else:
+            male.append(genero)
+    female= quicksort_1(female)
+    male= quicksort_1(male)
+    female=delete_dict(female)
+    male=delete_dict(male)
+    male_female=[]
+    if len(female) != 0:
+        male_female.append(female)
+    if len(male) != 0:
+        male_female.append(male)
+    print('La lista de edades y géneros, separados por género (quicksort) es:\n')
+    print(male_female)
+
+def delete_dict(lista):
+    pos=0
+    for i in lista:
+        temp=[]
+        edad=i['age']
+        genero=i['gender']
+        temp.append(edad)
+        temp.append(genero)
+        lista[pos]= temp
+        pos+=1
+    return lista
+
+#9. 
+def particionado_1(lista):
+    menores= []
+    mayores= []
+    saca_pivote= lista[-1]
+    pivote= saca_pivote['age']
+    for x in lista[:-1]:
+        if x['age'] < pivote:
+            menores.append(x)
+        else:
+            mayores.append(x)
+    return [menores, saca_pivote, mayores]
+
+def quicksort_1(lista):
+
+    if len(lista) > 0:
+        lista= particionado_1(lista)
+        cont= 0
+        while cont < len(lista):
+            elemento= lista[cont]
+            if type(elemento) == list:
+                if len(elemento) == 0:
+                    del lista[cont]
+                elif len(elemento) == 1:
+                    lista[cont]= elemento[0]
+                else:
+                    lista=lista[:cont]+ particionado_1(elemento)+ lista[cont+1:]
+            else:
+                cont+= 1
+        return lista
+
+def blonde_female(faces):                                    
+    blonde= 0
+    faceId1 = ''
+    estado = False
+    for face in faces:
+        faceId=face['faceId']
+        faceAttributes = face['faceAttributes']
+        gender = faceAttributes['gender']           
+        hair= faceAttributes['hair']
+        hairColor=hair['hairColor']
+        if gender == 'male':
+            continue
+        estado=True
+        for color in hairColor:
+            if color['color']=='blond':
+                color1=color['color']
+                color2=color['confidence']
+                break
+        if color2 > blonde:
+            blonde=color2
+            faceId1=faceId
+    if estado == True:
+        print("\nLa mujer más rubia es: ",faceId1,
+            "\n El porcentaje es de: ",blonde)
+    else:
+        print("\nNo se detectaron mujeres en la imagen") 
+
+def show_image(picture,
+        faces): 
+    image = Image.open(picture)
+    for face in faces:
+        fa = face['faceRectangle']
+        top = fa['top'] 
+        left = fa['left']  
+        width = fa['width'] 
+        height = fa['height']
+        draw = ImageDraw.Draw(image)
+        draw.rectangle((left, top, left+width, top+ height), outline= "blue", width = 4)
+    image.show(image)  
+
+def glasses_ungry(faces):
+    angry1 = 0
+    estado= False                                                        
+    faceId1 = " "        
+    for face in faces:
+        faceId=face['faceId']
+        faceAttributes = face['faceAttributes']
+        glasses = faceAttributes['glasses']
+        emotion = faceAttributes['emotion']  
+        angry=emotion['anger']
+        if glasses != 'NoGlasses':
+            estado= True
+            if angry >= angry1:   
+               angry1 = angry                                  
+               faceId1 = faceId    
+    if estado == True:
+        print("\n El más enojada/o es",faceId1,
+            "EL porcentaje de enojo es: ",angry1)
+    else:
+        print("\n No se detectaron personas con lentes ")      
+
+def historial_consultas(faces):  
+    historial=[]
+    for face in faces:
+        dicc={}
+        faceId=face['faceId']
+        faceAttributes=face['faceAttributes']
+        gender=faceAttributes['gender']
+        age=faceAttributes['age']
+        emotion=faceAttributes['emotion']
+        faceRectangle=face['faceRectangle']
+        dicc['faceId']= faceId
+        dicc['gender']= gender
+        dicc['age']= age
+        dicc['emotion']= emotion
+        dicc['faceRectangle']= faceRectangle
+        historial.append(dicc)
+    return historial
+
+def young_emotions_rec(picture, faces): 
+    mas_joven=150
+    faceId_real=""
+    emociones= {}
+    rostro_joven={}
+    for face in faces:
+        faceId=face['faceId']
+        faceRectangle=face['faceRectangle']
+        emotion=face['emotion']
+        age=face['age']
+        if age < mas_joven:
+            mas_joven=age
+            faceId_real= faceId
+            emociones=emotion
+            rostro_joven=face
+    print('\nLa persona más joven es:', faceId_real)
+    print('Edad:',mas_joven)
+    print('Emociones: ')
+    for x, y in emociones.items():
+            print('\t',x + ':', y)
+    show_image(picture, [rostro_joven])
+
+def show_faceId_and_hair_color(faces): 
+
+    print('El faceId y el color de cabello de las personas son:\n')
+    for face in faces:
+        faceId = face['faceId']
+        faceAttributes = face['faceAttributes']
+        hair = faceAttributes['hair']
+        hairColor = hair['hairColor']
+        if len(hairColor) > 0:
+            color = hairColor[0]
+            color1 = color['color']
+        else:
+            color1 = '¡No se detecto color de cabello en esta persona!'      
+        print("\nEl faceId es: ", faceId)
+        print("El color del cabello es: ",color1)   
+
+def show_accesories(faces): 
+    print('\n faceId y accesorios de cada persona:')
+    for face in faces:
+        faceId=face['faceId']
+        faceAttributes=face['faceAttributes']
+        accessories=faceAttributes['accessories']  
+        print("\nFaceId:",faceId)  
+        print('\nAccesorios:')
+        if len(accessories)== 0:
+            print("\t No tiene accesorios")
+        else:
+            for acce in accessories:
+                print('\t', acce['type'])
+
+def show_id_emotions(faces):     
+    print('el faceId y las emociones de las personas son:\n')
+    for face in faces:
+        faceId=face['faceId']
+        faceAttributes=face['faceAttributes']  
+        emotion=faceAttributes['emotion']
+        print('\nfaceId:', faceId)
+        print('\nEmociones:')
+        for x, y in emotion.items():
+            print('\t',x + ':', y) 
+
+
+
+if __name__ == "__main__":  
+    picture = input("Introduzca el path de la imagen: ")
+    information=emotions(picture)
+    hist=historial_consultas(information) 
+    young_emotions_rec(picture, hist)   
+    information=emotions(picture)
+    totalfaces=facesperimage(information)
+    masjoven(information)
+    metodosort(jovengenerosort(information))
+    agender10x10(metodosort(jovengenerosort(information)))
+    average=agesaverage(information)
+    metodoquicksort(faceIdgenderage(information))
+    happiness(information)
+    happyunderage(information,average)
+    beardandhappy(information)
+    show_age_and_gender_quick(information) 
+    blonde_female(information)
+    show_image(picture, information)
+    glasses_ungry(information) 
+    show_faceId_and_hair_color(information)
+    show_id_emotions(information) 
